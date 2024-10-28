3561760e10a64956be75e27e69fae8b4985f0f16
rafael <rafadevweb@gmail.com>
2024-10-28 14:40:00 -0300

    first commit

---
+++ geradorBlocosInf.js
@@ -0,0 +1,47 @@
+const geradorBlocoInf = document.getElementById("geradorBlocoInf");
+
+let move = 10;
+
+const paramove = () => {
+  move = 0;
+};
+
+const criarBloco = () => {
+  if (move > 0) {
+    const novoBloco = document.createElement("div");
+    novoBloco.style.width = "80px";
+    const alturaAleatoria = Math.floor(Math.random() * (250 - 100 + 1)) + 100;
+    novoBloco.style.height = alturaAleatoria + "px";
+    novoBloco.style.backgroundColor = "red";
+    novoBloco.style.position = "absolute";
+    const posicaoBase = 610;
+    novoBloco.style.top = `${posicaoBase - alturaAleatoria}px`;
+    novoBloco.style.left = "730px";
+    novoBloco.className = "bloco";
+    geradorBlocoInf.appendChild(novoBloco);
+
+    let posicaoLeft = 730;
+
+    const moverBlocox = setInterval(() => {
+      posicaoLeft -= move;
+      novoBloco.style.left = posicaoLeft + "px";
+
+      if (posicaoLeft > window.innerWidth) {
+        clearInterval(moverBlocox);
+        geradorBlocoInf.removeChild(novoBloco);
+      }
+    }, 50);
+
+    setTimeout(() => {
+      clearInterval(moverBlocox);
+      geradorBlocoInf.removeChild(novoBloco);
+    }, 4000);
+  }
+};
+
+setInterval(criarBloco, 1500);
+
+export default {
+  criarBloco: criarBloco,
+  paramove: paramove,
+};
