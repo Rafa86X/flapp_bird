2fa535e620fca0d296911a268b9c1081abb4c96c
rafael <rafadevweb@gmail.com>
2024-11-07 10:05:33 -0300

    testando sensibilidade de tela

--- bolinha.js
+++ bolinha.js
@@ -3,59 +3,53 @@ const tetaReverter = document.getElementById("tela");
 
 bolinha.style.left = "300px";
 bolinha.style.top = "550px";
-let posicaoTop = 200;
+let posicaoTop = 300;
 let paramoveAgora = false;
-let move = 10;
+let move = 0;
 
 bolinha.style.top = posicaoTop + "px";
 
 const paramove = () => {
   move = 0;
   paramoveAgora = true;
+  clearInterval(moverBolinha);
 };
 
 const moverBolinha = setInterval(() => {
   posicaoTop += move;
   bolinha.style.top = posicaoTop + "px";
 
-  if (posicaoTop > 550 || posicaoTop < 10) {
-    bolinha.style.top = "550px";
-    clearInterval(moverBolinha);
+  if (posicaoTop > 500 || posicaoTop < 120) {
+    move=0;
   }
-  if (posicaoTop < 10) {
-    bolinha.style.top = "10px";
-    clearInterval(moverBolinha);
-  }
+
+  
 }, 50);
 
-if (!paramoveAgora) {
-  tetaReverter.addEventListener("mousedown", () => {
-    if (!paramoveAgora) {
-      move = -10;
-    }
-  });
 
-  tetaReverter.addEventListener("mouseup", () => {
-    if (!paramoveAgora) {
-      move = 10;
-    }
-  });
+function iniciarMovimento(direcao) {
+  if (direcao === "cima" && bolinha.offsetTop > 119) {
+    move = -10;
+  } else if (direcao === "baixo" && bolinha.offsetTop < 499) {
+    move = 10;
+  }
+}
+
+
+if (!paramoveAgora) {
+  tela.addEventListener("mousedown", () => iniciarMovimento("cima"));
+  tela.addEventListener("mouseup", () => iniciarMovimento("baixo"));
 
   document.addEventListener("keydown", (event) => {
-    if (event.code === "Space" && !paramoveAgora) {
-      if (!paramoveAgora) {
-        move = -10;
-      }
-    }
+    if (event.code === "Space") iniciarMovimento("cima");
   });
 
   document.addEventListener("keyup", (event) => {
-    if (event.code === "Space" && !paramoveAgora) {
-      if (!paramoveAgora) {
-        move = 10;
-      }
-    }
+    if (event.code === "Space") iniciarMovimento("baixo");
   });
+
+  tela.addEventListener("touchstart", () => iniciarMovimento("cima"));
+  tela.addEventListener("touchend", () => iniciarMovimento("baixo"));
 }
 
 export default {
--- controlador.js
+++ controlador.js
@@ -7,30 +7,30 @@ let tempo = 0;
 const tempoJogo = setInterval(() => {
   tempo++;
 
-  switch (criaBlocos.getMove() > 0) {
-    case tempo < 2:
-      geradorBlocosInf.setaFase(0);
-      geradorBlocosSup.setaFase(0);
-      break;
+  // switch (criaBlocos.getMove() > 0) {
+  //   case tempo < 2:
+  //     geradorBlocosInf.setaFase(0);
+  //     geradorBlocosSup.setaFase(0);
+  //     break;
 
-    case tempo === 10:
-      geradorBlocosInf.setaFase(1);
-      geradorBlocosSup.setaFase(1);
-      break;
+  //   case tempo === 10:
+  //     geradorBlocosInf.setaFase(1);
+  //     geradorBlocosSup.setaFase(1);
+  //     break;
 
-    case tempo === 20:
-      geradorBlocosInf.setaFase(2);
-      geradorBlocosSup.setaFase(2);
-      break;
+  //   case tempo === 20:
+  //     geradorBlocosInf.setaFase(2);
+  //     geradorBlocosSup.setaFase(2);
+  //     break;
 
-    case tempo === 30:
-      geradorBlocosInf.setaFase(3);
-      geradorBlocosSup.setaFase(3);
-      break;
+  //   case tempo === 30:
+  //     geradorBlocosInf.setaFase(3);
+  //     geradorBlocosSup.setaFase(3);
+  //     break;
 
-    default:
-      break;
-  }
+  //   default:
+  //     break;
+  //}
 
   if (criaBlocos.getMove() < 1) {
     console.log("bateu, sequencia parada");
